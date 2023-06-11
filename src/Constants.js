4f3602130bbbefebf9318ca9b17322d2fbf89db3
Kasun Abaywardana <abaywardanakasun@gmail.com>
2023-06-11 22:13:38 +0800

    Added project list

--- src/Constants.js
+++ src/Constants.js
@@ -1,9 +1,54 @@
 export const PROJECTS = [
   {
-    name: "XGOWHERE",
+    name: "XGOWHERE - Senior Software Engineer",
     technologyUsed: "React JS, Node JS, Nest JS, CSS, AWS Lambda, Jest, AWS SDK, AWS S3, Cloud Watch, API Gateway, Bitbucket, Bamboo CI/CD, AWS CloudFront (CDN), Jira, Confluence",
     description: "A comprehensive set of web applications aims to aid the general public in Singapore in discovering places and acquiring knowledge about the diverse services offered by these establishments. The application comprises both back-end and front-end components, back-end based on serverless architecture.",
-  }
+  },
+  {
+    name: "TAFI - Associate Tech lead",
+    technologyUsed: "React JS, Node JS Express, mongo DB, Redis, SCSS, NX Console, Azure DevOps, VS Code, Jira, Confluence",
+    description: "A tax planning & lodgment application catered for individual income taxpayers, which automates the end-to-end process on the cloud. The application is comprised of three primary components: three web-based applications (Admin portal, Tax practice portal & client portal).",
+  },
+  {
+    name: "Decision Empowerment Application - Associate tech lead",
+    technologyUsed: "Angular 9, JavaScript, .NET Core, SQL Server, Azure, git, Jira, Confluence",
+    description: "Legacy Web application that comprises a data visualization tool at its core and a low-code suite for enterprises to support road-mapping, risk planning, and strategic decision-making.",
+  },
+  {
+    name: "AVATARIN - Senior Software Engineer",
+    technologyUsed: "React JS, Redux, MUI, Node JS, mongo db, JWT, SCSS, AWS EC2, AWS S3, Jira",
+    description: "The platform is a comprehensive web and mobile-based solution designed specifically for physically disabled individuals, facilitating seamless connections with robots located in public places. By leveraging the platform, users gain the ability to purchase dedicated time slots, enabling them to control and maneuver the robots effortlessly through an intuitive mobile or web application.",
+  },
+  {
+    name: "ONLINE BANKING SOLUTION - Senior Software Engineer",
+    technologyUsed: "Kony, JavaScript, Java, Fabric pre-processors, Bitbucket, Android studio",
+    description: "Online Banking solution for credit union (USA) customers to facilitate online banking and specific third-party services. The application is comprised of separate web and mobile components, both based on a customized JavaScript framework (Kony).",
+  },
+  {
+    name: "UPay - Senior Software Engineer",
+    technologyUsed: "Angular 6, SCSS, Cordova, Java 8, Spring boot, MySQL, Node JS, Firebase, Google Analytics, Google Cloud functions, Android Studio, X-code, Bitbucket, Jira, Confluence",
+    description: "UPay, Sri Lanka’s award-winning payments app gives you an innovative experience when accessing all your retail payment needs. Instant reloads, Utility bill payments, Book a movie ticket or a doctor's appointment, Online banking facility, and many more- all with just a few taps on your mobile or web.",
+  },
+  {
+    name: "Back-Office Admin Panel for Upay - Software Engineer",
+    technologyUsed: "React JS, Redux, JS PDF, SCSS, Java 8, Spring boot, MySQL, Bitbucket",
+    description: "The admin panel is all based on back-office operations of UPay payment wallet. The application's front-end is developed using React with MUI, while the back-end is built on Java (Spring Boot), Firebase, and MySQL. This is a support application for the previous banking Wallet.",
+  },
+  {
+    name: "BP-SYSTEM - Software Engineer",
+    technologyUsed: "React JS, Redux, MUI, SCSS, Java 8, Spring boot, MySQL, Bitbucket",
+    description: "The simple web-based platform allows business owners to create their own business processes, and users can manage their processes on the platform. This project's web application front-end is built on React, while the back-end is developed using Java (Spring Boot) and MySQL.",
+  },
+  {
+    name: "MOBILE POS - Software Engineer",
+    technologyUsed: "React JS, React Native, Redux, SCSS, Java 8, Spring boot, MySQL, Bitbucket",
+    description: "Mobile-based POS system for QR payments. In this project, the mobile application front-end is built on React Native, while the admin panel is developed using React. The back-end is built on Java (Spring Boot) and MySQL.",
+  },
+  {
+    name: "Grade5 (Examination Portal) - Associate Software Engineer",
+    technologyUsed: "Angular 2, CSS, Node JS, mongo db, Android, Java, Bitbucket",
+    description: "An educational portal to Sri Lankan Grade 5 students. Students can buy and do the papers at a given time frame and see the marks and answers after the exam and parents can analyze children’s progress according to their results. The system had several components including client frontend, API, admin panel, Android and IOS mobile apps.",
+  },
 ];
 
 export const SKILLS = [
